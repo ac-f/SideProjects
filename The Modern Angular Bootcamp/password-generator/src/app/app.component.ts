2beb5a820d23939e04e3e2e2a88a3cdf276f57c7
張明輝 <torichang@local>
2022-01-22 14:42:33 +0800

    finish password generator

--- The Modern Angular Bootcamp/password-generator/src/app/app.component.ts
+++ The Modern Angular Bootcamp/password-generator/src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  password: string = '';
+
+  checkBtnValid(){
+    return (!this.includeLetters && !this.includeNumbers && !this.includeSymbols) || this.length <= 0;
+  }
+  onChangeLength(event: Event) {
+    const eventValue = (event.target as HTMLInputElement).value;
+    if (isNaN(+eventValue)) {
+      length = 0;
+    }
+    this.length = +eventValue;
+
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onGeneratePassword() {
+    const numbers: string = '0123456789';
+    const letters: string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols: string = '!@#$%^&*()';
+    let validChars: string = '';
+    let generatedPassword: string = '';
+    if (this.includeLetters) validChars += letters;
+    if (this.includeNumbers) validChars += numbers;
+    if (this.includeSymbols) validChars += symbols;
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
